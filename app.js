f7f473f3c7a0e0fa23eb42d311fd73c257a296db
Win Cheng <wincheng@Wins-MacBook-Pro.local>
2023-02-02 06:17:06 -0800

    Combined all JS Files and added Transition delay to Events

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const primary_Nav = document.querySelector('.nav_links');
+const nav_Toggle = document.querySelector('.mobile-nav-toggle');
+
+nav_Toggle.addEventListener('click', () => {
+    const visibility = primary_Nav.getAttribute('data-visible');
+
+    if (visibility === "false"){
+        primary_Nav.setAttribute('data-visible', true); 
+        nav_Toggle.setAttribute('aria-expanded', true);
+    } else {
+        nav_Toggle.setAttribute('aria-expanded', false);
+        primary_Nav.setAttribute('data-visible', false);
+    }
+
+});
+
 
 
 const observer = new IntersectionObserver((entries) => {
@@ -13,3 +29,33 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".events-hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+let preveiwContainer = document.querySelector('.profile-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+function CloseFunction(){
+    let preview = document.querySelector('.active');
+    preview.classList.remove('active');
+
+}
+
+function OpenFunction(){
+    let name = this.getAttribute('data-name');
+    previewBox.forEach(preview => {
+        let target = preview.getAttribute('data-target');
+        if(name == target){
+            preview.classList.add('active');
+        }
+    })
+
+
+}
+
+document.getElementById('Chin').onclick = OpenFunction;
+document.getElementById('Gavin').onclick = OpenFunction;
+document.getElementById('Glory').onclick = OpenFunction;
+document.getElementById('Win').onclick = OpenFunction;
+document.getElementById('Bow').onclick = OpenFunction;
+document.getElementById('Minnie').onclick = OpenFunction;
+document.getElementById('Summer').onclick = OpenFunction;
+document.getElementById('Eik').onclick = OpenFunction;
